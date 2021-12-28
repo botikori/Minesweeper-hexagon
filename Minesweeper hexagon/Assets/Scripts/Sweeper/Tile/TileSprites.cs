3bb527c6a54a36c2ebf3bfac98cbd9e8b5052bc6
botikori <54451878+botikori@users.noreply.github.com>
2021-12-28 16:29:36 +0100

    Tile sprites added

--- Minesweeper hexagon/Assets/Scripts/Sweeper/Tile/TileSprites.cs
+++ Minesweeper hexagon/Assets/Scripts/Sweeper/Tile/TileSprites.cs
@@ -4,6 +4,22 @@ namespace Sweeper
 {
     public class TileSprites : MonoBehaviour
     {
-        
+        [SerializeField] private Sprite[] revealedNumbers;
+        [SerializeField] private Sprite revealedMine;
+        [SerializeField] private Sprite revealedEmpty;
+        [SerializeField] private Sprite revealedMineDeath;
+        [SerializeField] private Sprite revealedMineFlag;
+        [SerializeField] private Sprite unrevealed;
+        [SerializeField] private Sprite unrevealedFlag;
+        [SerializeField] private Sprite unrevealedQuestion;
+
+        public Sprite[] RevealedNumbers { get { return revealedNumbers; } }
+        public Sprite RevealedMine { get { return revealedMine; } }
+        public Sprite RevealedEmpty { get { return revealedEmpty; } }
+        public Sprite RevealedMineDeath { get { return revealedMineDeath; } }
+        public Sprite RevealedMineFlag { get { return revealedMineFlag; } }
+        public Sprite Unrevealed { get { return unrevealed; } }
+        public Sprite UnrevealedFlag { get { return unrevealedFlag; } }
+        public Sprite UnrevealedQuestion { get { return unrevealedQuestion; } }
     }
 }
